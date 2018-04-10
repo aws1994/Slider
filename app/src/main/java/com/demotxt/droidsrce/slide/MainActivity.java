bb964d8e0a386fdc3567a7308973c9e5752ce393
aws1994 <awsrhimi@gmail.com>
2018-04-10 13:46:44 +0100

    Initial commit

---
+++ app/src/main/java/com/demotxt/droidsrce/slide/MainActivity.java
@@ -0,0 +1,19 @@
+package com.demotxt.droidsrce.slide;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+
+    }
+}
---
+++ app/src/main/java/com/demotxt/droidsrce/slide/SlideAdapter.java
@@ -0,0 +1,82 @@
+package com.demotxt.droidsrce.slide;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SlideAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater inflater;
+
+    // list of images
+    public int[] lst_images = {
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_3,
+            R.drawable.image_4
+    };
+    // list of titles
+    public String[] lst_title = {
+            "COSMONAUT",
+            "SATELITE",
+            "GALAXY",
+            "ROCKET"
+    }   ;
+    // list of descriptions
+    public String[] lst_description = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
+    };
+    // list of background colors
+    public int[]  lst_backgroundcolor = {
+            Color.rgb(55,55,55),
+            Color.rgb(239,85,85),
+            Color.rgb(110,49,89),
+            Color.rgb(1,188,212)
+    };
+
+
+    public SlideAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return lst_title.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view==(LinearLayout)object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slide,container,false);
+        LinearLayout layoutslide = (LinearLayout) view.findViewById(R.id.slidelinearlayout);
+        ImageView imgslide = (ImageView)  view.findViewById(R.id.slideimg);
+        TextView txttitle= (TextView) view.findViewById(R.id.txttitle);
+        TextView description = (TextView) view.findViewById(R.id.txtdescription);
+        layoutslide.setBackgroundColor(lst_backgroundcolor[position]);
+        imgslide.setImageResource(lst_images[position]);
+        txttitle.setText(lst_title[position]);
+        description.setText(lst_description[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
